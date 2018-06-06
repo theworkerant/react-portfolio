2e19dee0a79a887e0a2a30b39b9be707d9167370
nigelflippo <flippo.nigel@gmail.com>
2018-06-06 14:56:04 -0600

    quick commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {
   container,
-  item
+  item,
+  header
 } from './styles/Footer.css'
 import { Link } from 'react-router-dom'
 
@@ -11,8 +12,12 @@ class Footer extends Component {
   render() {
     return (
       <div className={container}>
-        <span className={item}>flippo.nigel@gmail.com</span>
+        <span className={header}>Full-Stack Portfolio</span>
+        <span className={item}>built using:</span>
+        <span className={item}>React | Redux | Webpack | NodeJS | Express</span>
+        <br/>
         {/* <span className={item}>2000 Walnut St, Apt 103<br/>Boulder CO, 80302</span> */}
+        <span className={item}>flippo.nigel@gmail.com</span>
       </div>
     )
   }
